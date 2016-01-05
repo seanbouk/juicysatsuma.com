8bc26921d8a00cce1ee9e82faf56de738be1b5d0
Sean Thompson <seanbo.uk.talk21.com@gmail.com>
2016-01-05 21:43:23 +0000

    Initial Commit of the half life calculator
    Calculates half life
    All buttons working
    Seems stable
    Social buttons point at the correct page
    ...not sure about comments section. Prolly not.

---
+++ halflife/js/script.js
@@ -0,0 +1,141 @@
+var n0;
+var nT;
+var t;
+var halflife;
+var n1;
+
+$(document).ready(function()
+{
+	//load();
+	update();
+
+	$('.decrement').click(function()
+	{
+		var target = $("#" + $(this).data("target"));
+		target.val() >= 1 ? target.val(target.val()-1) : target.val(0);
+
+		update();
+	});
+
+	$('.increment').click(function()
+	{
+		var target = $("#" + $(this).data("target"));
+		target.val(target.val()-(-1));//beautiful filth
+
+		update();
+	});
+
+	$('.estimate').on('input', function() {
+		update();
+	});
+
+	$("#confidence").on('input', function()
+	{
+		update();
+	});
+
+	(function ( $ ) {
+	    var filters = $.expr[":"];
+	    if ( !filters.focus ) { 
+	        filters.focus = function( elem ) {
+	           return elem === document.activeElement && ( elem.type || elem.href );
+	        };
+	    }
+	})( jQuery );
+});
+
+function update()
+{
+	cleanNumericalField($("#N0"));
+	cleanNumericalField($("#NT"));
+	cleanNumericalField($("#T"));
+
+	//save();
+
+	calculateHalfLife();
+	//findX();
+}
+
+function save()
+{
+	$.cookie("costingsBest", Number($("#best").val()), { expires: 365*10 });
+	$.cookie("costingsLikely", Number($("#likely").val()), { expires: 365*10 });
+	$.cookie("costingsWorst", Number($("#worst").val()), { expires: 365*10 });
+
+	$.cookie("costingsConfidence", Number($("#confidence").val()), { expires: 365*10 });
+}
+
+function load()
+{
+	if($.cookie("costingsBest")) $("#best").val($.cookie("costingsBest"));
+	if($.cookie("costingsLikely")) $("#likely").val($.cookie("costingsLikely"));
+	if($.cookie("costingsWorst")) $("#worst").val($.cookie("costingsWorst"));
+
+	if($.cookie("costingsConfidence")) $("#confidence").val($.cookie("costingsConfidence"));
+}
+
+function cleanNumericalField(targetField)
+{
+	var caret = targetField.caret();
+
+	//digits dots and dashes only
+	targetField.val(targetField.val().replace(/([^0-9\-\.])/g, ""));
+
+	//no dashes after the first character
+	targetField.val(targetField.val().replace(/(?!^)-/g, ""));
+
+	//only one dot (no lookbehind in js)
+	var dotsArray = targetField.val().split(".");
+	if(dotsArray.length > 1)
+	{
+		dotsArray[0] += ".";
+		targetField.val(dotsArray.join(""));
+	}
+
+	//dot at the beginning gets a zero in front
+	targetField.val(targetField.val().replace(/^\./g, "0."));
+
+	//no zeroes before a number, only one before a dot
+	targetField.val(targetField.val().replace(/^0+(?=\d)/g, ""));//+ve
+	targetField.val(targetField.val().replace(/^-0+(?=\d)/g, "-"));//-ve
+
+	//leading -.
+	targetField.val(targetField.val().replace("-.", "-0."));
+
+	if(targetField.is(":focus")) targetField.caret(caret);
+
+}
+
+function hasFocus(elem) 
+{
+  return elem === document.activeElement && (elem.type || elem.href);
+}
+
+function calculateHalfLife()
+{
+	n0 = Number($("#N0").val());
+	nT = Number($("#NT").val());
+	t = Number($("#T").val());
+
+	halflife = (t * Math.log(2)) / Math.log(n0/nT);
+	$(".result").html(halflife.toPrecision(3));
+}
+
+function findX()
+{
+	var target = Number($("#confidence").val());
+
+	var min = 0.0;
+	var max = 3 * worst;//2*worst reliiably gives you up to about 99% chance. 3*worst covers you.
+
+	var test;
+	var mid;
+	for(var i=0; i<20; i++)
+	{
+		mid = (min+max)/2;
+		test = foldedNormalCDF(mid, mean, sd);
+		test < target ? min = mid : max = mid;
+	}
+
+	$(".result").html(min.toPrecision(3));
+}
